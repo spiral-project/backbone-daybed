b8163f244bfd6deba8e926ae13fa0cc9c056dfd0
Mathieu Leplatre <contact@mathieu-leplatre.info>
2013-02-06 16:55:33 +0100

    Start using routers

--- app.js
+++ app.js
@@ -21,8 +21,15 @@ var MushroomSpot = Backbone.Model.extend({
 });
 
 var MushroomSpotList = Backbone.Collection.extend({
-    url: URI.build({hostname:settings.SERVER, path: '/data/mushroomspot'}),
     model: MushroomSpot,
+
+	initialize: function (modelname) {
+		this.modelname = modelname;
+	},
+
+	url: function () {
+		return URI.build({hostname:settings.SERVER, path: '/data/' + this.modelname});
+    },
 
     parse: function(response) {
         return response.data;
@@ -105,25 +112,34 @@ var MushroomSpotCreate = Backbone.View.extend({
     },
 
     showErrors: function (model, xhr, options) {
-        var descriptions = JSON.parse(xhr.responseText),
-            self = this;
-        $(descriptions.errors).each(function (i, e) {
-            self.$el.find("[name='" + e.name + "']")
-                .after(self.templateError({msg: e.description}));
-        });
+        try {
+			var descriptions = JSON.parse(xhr.responseText),
+				self = this;
+			$(descriptions.errors).each(function (i, e) {
+				self.$el.find("[name='" + e.name + "']")
+					.after(self.templateError({msg: e.description}));
+			});
+		}
+		catch (e) {
+			this.$el.html(this.templateError({msg: xhr.responseText}));
+		}
     },
 });
 
 
 var DefinitionCreate = Backbone.View.extend({
-	template: Mustache.compile('<h2>Create definition</h2><form><textarea>{"title":"mushroomspots","description":"mushroom spots", "fields": [{"name":"mushroom","type":"string","description":"what"},{"name":"area","type":"point","description":"where"}]}</textarea><button id="create">Create</button></form>'),
+	template: Mustache.compile('<h2>Create definition of "{{ modelname }}"</h2><form><textarea>{"title":"mushroomspots","description":"mushroom spots", "fields": [{"name":"mushroom","type":"string","description":"what"},{"name":"area","type":"point","description":"where"}]}</textarea><button id="create">Create</button></form>'),
 
     events: {
         "click button#create": "formSubmitted",
     },
 
+	initialize: function (modelname) {
+		this.modelname = modelname;
+	},
+
     render: function () {
-		this.$el.html(this.template({}));
+		this.$el.html(this.template({modelname: this.modelname}));
         this.delegateEvents();
         return this;
     },
@@ -133,7 +149,7 @@ var DefinitionCreate = Backbone.View.extend({
         var definition = this.$el.find('textarea').val();
         $.ajax({
 		  type: "PUT",
-		  url: URI.build({hostname:settings.SERVER, path: '/definitions/mushroomspot'}),
+		  url: URI.build({hostname:settings.SERVER, path: 'definitions/' + this.modelname}),
 		  data: definition,
 		  contentType: 'application/json',
 		  dataType: 'json',
@@ -143,68 +159,78 @@ var DefinitionCreate = Backbone.View.extend({
     },
 
 	success: function () {
-		window.location = '/';  // Use backbone routers :)
+		app.navigate(this.modelname + '/list', {trigger:true});
 	},
 });
 
 
-var AppView = Backbone.View.extend({
+var ListView = Backbone.View.extend({
+	template: Mustache.compile('<h1>{{ modelname }}</h1><div id="toolbar"><a href="#{{ modelname }}/add">Add</a></div>' + 
+	                           '<div id="list"></div><div id="footer">{{ count }} items.</div>'),
 
-    el: $('#content').parent(),
-    statsTemplate: Mustache.compile('{{ count }} items.'),
+    initialize: function (map, modelname) {
+		this.map = map;
+		this.modelname = modelname;
 
-    events: {
-        "click #create": "createOne",
-    },
-
-    initialize: function () {
-        this.map = L.map('map').setView([0, 0], 3);
-        this.map.attributionControl.setPrefix(''); 
-        L.tileLayer('http://{s}.tiles.mapbox.com//v3/leplatrem.map-3jyuq4he/{z}/{x}/{y}.png').addTo(this.map);
-        this.bounds = new L.LatLngBounds();
-
-        this.footer = $('#footer');
-        this.createView = new MushroomSpotCreate(this.map);
+		MushroomSpots.modelname = modelname; // beurk
         MushroomSpots.bind('add', this.addOne, this);
         MushroomSpots.bind('reset', this.addAll, this);
-        MushroomSpots.bind('all', this.render, this);
         MushroomSpots.bind('error', this.handleError, this);
         MushroomSpots.fetch();
     },
 
-    handleError: function(model, error) {
-        if (error.status == 404) {
-            this.$("#content").html(new DefinitionCreate().render().el);
+    handleError: function(model, xhr, options) {
+        if (xhr.status == 404) {
+            app.navigate(this.modelname + '/create', {trigger:true});
         }
     },
 
     render: function () {
-        var count = MushroomSpots.length;
-        if (count > 0) {
-            this.map.fitBounds(this.bounds);
-            this.footer.show();
-            this.footer.html(this.statsTemplate({count: count}));
-        }
-        else {
-            this.footer.hide();
-        }
+		var count = MushroomSpots.length;
+        this.$el.html(this.template({modelname: this.modelname, count:count}));
         return this;
     },
 
-    createOne: function(e) {
-        this.$("#content").prepend(this.createView.render().el);
-    },
-
     addOne: function (spot) {
-        self = App; // WTF ?
         var view = new MushroomSpotRow({model: spot});
-        this.$('#content').append(view.render().el);
+        this.$('#list').append(view.render().el);
         var geom = spot.geometry();
-        geom.addTo(self.map);
-        self.bounds.extend(geom.getLatLng());
+        geom.addTo(this.map);
+        this.bounds.extend(geom.getLatLng());
     },
 
     addAll: function () {
-        MushroomSpots.each(this.addOne);
+		this.render();
+		this.bounds = new L.LatLngBounds();
+        MushroomSpots.each(this.addOne.bind(this));
+        if (this.bounds.isValid()) this.map.fitBounds(this.bounds);
     }
+});
+
+
+var DaybedMapApp = Backbone.Router.extend({
+
+	routes: {
+		":modelname/create":   "create",
+		":modelname/list":     "list",
+		":modelname/add":      "add",
+	},
+
+	initialize: function () {
+        this.map = L.map('map').setView([0, 0], 3);
+        this.map.attributionControl.setPrefix(''); 
+        L.tileLayer('http://{s}.tiles.mapbox.com//v3/leplatrem.map-3jyuq4he/{z}/{x}/{y}.png').addTo(this.map);
+	},
+
+	create: function(modelname) {
+		$("#content").html(new DefinitionCreate(modelname).render().el);
+	},
+
+	list: function(modelname) {
+		$("#content").html(new ListView(this.map, modelname).render().el);
+	},
+
+	add: function(modelname) {
+		$("#content #list").prepend(new MushroomSpotCreate(this.map).render().el);
+	}
 });

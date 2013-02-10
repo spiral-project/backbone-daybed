08a49db58ec0050af4d060eeb1201876ad012352
Mathieu Leplatre <contact@mathieu-leplatre.info>
2013-02-10 22:26:21 +0100

    Tabs as spaces

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ var Item = Backbone.Model.extend({
 var ItemList = Backbone.Collection.extend({
     model: Item,
 
-	initialize: function (modelname) {
-		this.modelname = modelname;
-	},
+    initialize: function (modelname) {
+        this.modelname = modelname;
+    },
 
-	url: function () {
-		return URI.build({hostname:settings.SERVER, path: '/data/' + this.modelname});
+    url: function () {
+        return URI.build({hostname:settings.SERVER, path: '/data/' + this.modelname});
     },
 
     parse: function(response) {
@@ -49,7 +49,7 @@ var ItemRow = Backbone.View.extend({
 });
 
 var FormView = Backbone.View.extend({
-	templateError: Mustache.compile('<span class="field-error">{{ msg }}</span>'),
+    templateError: Mustache.compile('<span class="field-error">{{ msg }}</span>'),
     
     events: {
         "submit form": "formSubmitted",
@@ -57,7 +57,7 @@ var FormView = Backbone.View.extend({
     },
 
     render: function () {
-		this.$el.html(this.template(this));
+        this.$el.html(this.template(this));
         this.delegateEvents();
         return this;
     },
@@ -76,16 +76,16 @@ var FormView = Backbone.View.extend({
 
     showErrors: function (model, xhr, options) {
         try {
-			var descriptions = JSON.parse(xhr.responseText),
-				self = this;
-			$(descriptions.errors).each(function (i, e) {
-				self.$el.find("[name='" + e.name + "']")
-					.after(self.templateError({msg: e.description}));
-			});
-		}
-		catch (e) {
-			this.$el.html(this.templateError({msg: xhr.responseText}));
-		}
+            var descriptions = JSON.parse(xhr.responseText),
+                self = this;
+            $(descriptions.errors).each(function (i, e) {
+                self.$el.find("[name='" + e.name + "']")
+                    .after(self.templateError({msg: e.description}));
+            });
+        }
+        catch (e) {
+            this.$el.html(this.templateError({msg: xhr.responseText}));
+        }
     },
 });
 
@@ -115,13 +115,13 @@ var AddView = FormView.extend({
     },
 
     formCancelled: function (e) {
-		FormView.prototype.formCancelled.apply(this, arguments);
+        FormView.prototype.formCancelled.apply(this, arguments);
         this.close();
         return false;
     },
 
     formSubmitted: function(e) {
-		FormView.prototype.formSubmitted.apply(this, arguments);
+        FormView.prototype.formSubmitted.apply(this, arguments);
         this.collection.create(this.data, {
             wait: true,
             error: this.showErrors.bind(this),
@@ -145,23 +145,23 @@ var AddView = FormView.extend({
 
 var Model = Backbone.Model.extend({
     url: function () {
-		return URI.build({hostname:settings.SERVER, path: 'definitions/' + this.id});
-	},
+        return URI.build({hostname:settings.SERVER, path: 'definitions/' + this.id});
+    },
 });
 
 
 var ModelCreate = FormView.extend({
-	template: Mustache.compile('<h2>Create model "{{ modelname }}"</h2><form>' +
-							   '<input type="hidden" name="id" value="{{ modelname }}"/>' +
-	                           '<input type="text" name="title" placeholder="title"/>' +
-	                           '<input type="text" name="description" placeholder="description"/>' +
-	                           '<textarea name="fields[]">{"name":"mushroom","type":"string","description":"what"}</textarea>' +
-	                           '<textarea name="fields[]">{"name":"area","type":"point","description":"where"}</textarea>' +
-	                           '<button type="submit">Create</button></form>'),
+    template: Mustache.compile('<h2>Create model "{{ modelname }}"</h2><form>' +
+                               '<input type="hidden" name="id" value="{{ modelname }}"/>' +
+                               '<input type="text" name="title" placeholder="title"/>' +
+                               '<input type="text" name="description" placeholder="description"/>' +
+                               '<textarea name="fields[]">{"name":"mushroom","type":"string","description":"what"}</textarea>' +
+                               '<textarea name="fields[]">{"name":"area","type":"point","description":"where"}</textarea>' +
+                               '<button type="submit">Create</button></form>'),
 
-	initialize: function (modelname) {
-		this.modelname = modelname;
-	},
+    initialize: function (modelname) {
+        this.modelname = modelname;
+    },
 
     formSubmitted: function(e) {
         FormView.prototype.formSubmitted.apply(this, arguments);
@@ -174,26 +174,26 @@ var ModelCreate = FormView.extend({
         return false;
     },
 
-	success: function () {
-		app.navigate(this.modelname + '/list', {trigger:true});
-	},
+    success: function () {
+        app.navigate(this.modelname + '/list', {trigger:true});
+    },
 });
 
 
 var ListView = Backbone.View.extend({
-	template: Mustache.compile('<h1>{{ modelname }}</h1><div id="toolbar"><a href="#{{ modelname }}/add">Add</a></div>' + 
-	                           '<div id="list"></div><div id="footer">{{ count }} items.</div>'),
+    template: Mustache.compile('<h1>{{ modelname }}</h1><div id="toolbar"><a href="#{{ modelname }}/add">Add</a></div>' + 
+                               '<div id="list"></div><div id="footer">{{ count }} items.</div>'),
 
     initialize: function (map, collection) {
-		this.map = map;
-		this.collection = collection;
+        this.map = map;
+        this.collection = collection;
 
         collection.bind('add', this.addOne, this);
         collection.bind('reset', this.addAll, this);
     },
 
     render: function () {
-		var count = this.collection.length;
+        var count = this.collection.length;
         this.$el.html(this.template({modelname: this.collection.modelname, count:count}));
         return this;
     },
@@ -207,8 +207,8 @@ var ListView = Backbone.View.extend({
     },
 
     addAll: function () {
-		this.render();
-		this.bounds = new L.LatLngBounds();
+        this.render();
+        this.bounds = new L.LatLngBounds();
         this.collection.each(this.addOne.bind(this));
         if (this.bounds.isValid()) this.map.fitBounds(this.bounds);
     }
@@ -217,41 +217,41 @@ var ListView = Backbone.View.extend({
 
 var DaybedMapApp = Backbone.Router.extend({
 
-	routes: {
-		":modelname/create":   "create",
-		":modelname/list":     "list",
-		":modelname/add":      "add",
-	},
+    routes: {
+        ":modelname/create":   "create",
+        ":modelname/list":     "list",
+        ":modelname/add":      "add",
+    },
 
-	initialize: function () {
-		this.collection = null;
-		
+    initialize: function () {
+        this.collection = null;
+        
         this.map = L.map('map').setView([0, 0], 3);
         this.map.attributionControl.setPrefix(''); 
         L.tileLayer('http://{s}.tiles.mapbox.com//v3/leplatrem.map-3jyuq4he/{z}/{x}/{y}.png').addTo(this.map);
-	},
+    },
 
-	create: function(modelname) {
-		$("#content").html(new ModelCreate(modelname).render().el);
-	},
+    create: function(modelname) {
+        $("#content").html(new ModelCreate(modelname).render().el);
+    },
 
-	list: function(modelname) {
-		if (!this.collection || this.collection.modelname != modelname) {
-			this.collection = new ItemList(modelname);
-			var createIfMissing = function (model, xhr) {
-				if (xhr.status == 404) {
-					app.navigate(modelname + '/create', {trigger:true});
-				}
-			};
-			this.collection.bind('error', createIfMissing, this);
-		}
-		$("#content").html(new ListView(this.map, this.collection).render().el);
-		this.collection.fetch();
-	},
+    list: function(modelname) {
+        if (!this.collection || this.collection.modelname != modelname) {
+            this.collection = new ItemList(modelname);
+            var createIfMissing = function (model, xhr) {
+                if (xhr.status == 404) {
+                    app.navigate(modelname + '/create', {trigger:true});
+                }
+            };
+            this.collection.bind('error', createIfMissing, this);
+        }
+        $("#content").html(new ListView(this.map, this.collection).render().el);
+        this.collection.fetch();
+    },
 
-	add: function(modelname) {
-		if (!this.collection|| this.collection.modelname != modelname)
-			this.list(modelname);
-		$("#content #list").prepend(new AddView(this.map, this.collection).render().el);
-	}
+    add: function(modelname) {
+        if (!this.collection|| this.collection.modelname != modelname)
+            this.list(modelname);
+        $("#content #list").prepend(new AddView(this.map, this.collection).render().el);
+    }
 });

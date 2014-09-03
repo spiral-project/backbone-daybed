153c1f523b0524eecda7852a62b0f82e107f9fd5
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-09-03 22:44:50 +0200

    Refresh on hash change

--- main.js
+++ main.js
@@ -1,52 +1,67 @@
-Daybed.SETTINGS.SERVER = "https://daybed.lolnet.org";
+function init() {
 
-//
-// By default, load todo model anonymously
-//
-var hash = window.location.hash.slice(1) || 'todo';
-window.location.hash = hash;
+    //
+    // By default, load todo model anonymously
+    //
+    Daybed.SETTINGS.SERVER = "https://daybed.lolnet.org";
 
-//
-// Instantiate Models and Views
-//
-var definition = new Daybed.Definition({id: id});
-var recordList = new Daybed.RecordList(definition);
+    var hash = window.location.hash.slice(1) || 'todo';
+    window.location.hash = hash;
 
-// Instantiate views
-var listView = new Daybed.TableView({collection: recordList});
-var formView = new Daybed.FormView({definition: definition});
+    var credentials = hash.split(':');
+    var modelId = credentials[0];
+    Daybed.SETTINGS.credentials = {
+        id: credentials[1],
+        key: credentials[2],
+        algorithm: "sha256"
+    };
 
-// Wait for definition to be fetched from server
-// in order to populate form fields and list headers
-definition.whenReady(function () {
-    $('#records-list').html(listView.render().el);
-    $('#form-holder').html(formView.render().el);
-});
+    //
+    // Instantiate Models and Views
+    //
+    var definition = new Daybed.Definition({id: modelId});
+    var recordList = new Daybed.RecordList(definition);
 
-// Fetch definition !
-definition.fetch();
-// Fetch records !
-recordList.fetch();
+    // Instantiate views
+    var listView = new Daybed.TableView({collection: recordList});
+    var formView = new Daybed.FormView({definition: definition});
 
-//
-// Cable list and form events
-//
-// Rebuild the form with bound instance on edit.
-listView.on('edit', function (record, row) {
-    $('tr.info').removeClass('info');
-    row.$el.addClass('info');
-    formView.setup(record);
-    formView.render();
-});
+    // Wait for definition to be fetched from server
+    // in order to populate form fields and list headers
+    definition.whenReady(function () {
+        $('#records-list').html(listView.render().el);
+        $('#form-holder').html(formView.render().el);
+    });
 
-// On creation, add to the collection.
-formView.on('created', function (record) {
-    recordList.add(record);
-});
+    // Fetch definition !
+    definition.fetch();
 
-// After cancel or edition, empty the form.
-formView.on('cancel saved created', function () {
-    formView.setup(null);
-    formView.render();
-    $('tr.info').removeClass('info');
-});
+    // Fetch records !
+    recordList.fetch();
+
+    //
+    // Cable list and form events
+    //
+    // Rebuild the form with bound instance on edit.
+    listView.on('edit', function (record, row) {
+        $('tr.info').removeClass('info');
+        row.$el.addClass('info');
+        formView.setup(record);
+        formView.render();
+    });
+
+    // On creation, add to the collection.
+    formView.on('created', function (record) {
+        recordList.add(record);
+    });
+
+    // After cancel or edition, empty the form.
+    formView.on('cancel saved created', function () {
+        formView.setup(null);
+        formView.render();
+        $('tr.info').removeClass('info');
+    });
+}
+
+init();
+window.onhashchange = init;
